5bae7f93dd821c9e5a2f9d5645b3a86ddeadfb3c
MaksRog <mexarms@gmail.com>
2021-06-23 08:10:47 +0300

    implemented image loading

--- app/src/main/java/com/mraha/imagesearchapp/ui/gallery/GalleryAdapter.kt
+++ app/src/main/java/com/mraha/imagesearchapp/ui/gallery/GalleryAdapter.kt
@@ -3,13 +3,9 @@ package com.mraha.imagesearchapp.ui.gallery
 import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
-import android.content.ContentResolver
 import android.content.ContentValues
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
-import android.net.Uri
-import android.os.Build
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -21,9 +17,11 @@ import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.transition.Transition
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.mraha.imagesearchapp.R
 import com.mraha.imagesearchapp.data.Photo
 import com.mraha.imagesearchapp.database.AppDatabase
@@ -31,11 +29,7 @@ import com.mraha.imagesearchapp.databinding.ItemUnsplashPhotoBinding
 import com.mraha.imagesearchapp.di.MEntryPoint
 import dagger.hilt.android.EntryPointAccessors
 import kotlinx.coroutines.launch
-import java.io.File
-import java.io.FileOutputStream
 import java.io.IOException
-import java.io.OutputStream
-import java.util.*
 
 
 class GalleryAdapter(
@@ -126,46 +120,82 @@ class GalleryAdapter(
                         )
                     } else {
                         Glide.with(fragment.requireContext())
-                            .asFile()
-                            .load(getItem(position))
-                            .into(object : CustomTarget<File>() {
-
-
-                                override fun onLoadCleared(placeholder: Drawable?) {
+                            .asBitmap()
+                            .load(getItem(position)?.url)
+                            .listener(object : RequestListener<Bitmap> {
+                                override fun onLoadFailed(
+                                    e: GlideException?,
+                                    model: Any?,
+                                    target: Target<Bitmap>?,
+                                    isFirstResource: Boolean
+                                ): Boolean {
+                                    return false
                                 }
 
                                 override fun onResourceReady(
-                                    resource: File,
-                                    transition: Transition<in File>?
-                                ) {
-                                    val resolver = fragment.requireContext().contentResolver
-                                    ContentValues().apply {
-                                        put(MediaStore.Audio.Media.DISPLAY_NAME, "My Song.mp3")
-                                    }
-                                }
+                                    resource: Bitmap?,
+                                    model: Any?,
+                                    target: Target<Bitmap>?,
+                                    dataSource: DataSource?,
+                                    isFirstResource: Boolean
+                                ): Boolean {
+                                    if (resource != null) {
+                                        if (savePhotoToExternalStorage("temp", resource)) {
+                                            fragment.requireActivity()
+                                                .runOnUiThread {
+                                                    Toast.makeText(
+                                                        fragment
+                                                            .requireContext(),
+                                                        "Image saved to downloads",
+                                                        Toast.LENGTH_LONG
+                                                    ).show()
+                                                }
+                                            return true
+                                        } else {
+                                            fragment.requireActivity().runOnUiThread {
+                                                Toast.makeText(
+                                                    fragment
+                                                        .requireContext(),
+                                                    "Error while saving image",
+                                                    Toast.LENGTH_LONG
+                                                ).show()
+                                            }
 
-                            })
+                                        }
+                                    }
+                                    return false
+                                }
+                            }).submit()
                     }
                 }
             }
         }
-
         return builder.create()
     }
 
-    @Throws(IOException::class)
-    private fun saveImage(bitmap: Bitmap, name: String) {
-        val fos: OutputStream?
-        val resolver: ContentResolver = fragment.requireContext().contentResolver
-        val contentValues = ContentValues()
-        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "$name.jpg")
-        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
-        val imageUri: Uri? =
-            resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-        fos = resolver.openOutputStream(Objects.requireNonNull(imageUri))
+    private fun savePhotoToExternalStorage(displayName: String, bmp: Bitmap): Boolean {
+        val imageCollection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
 
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-        Objects.requireNonNull(fos).close()
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "$displayName.jpg")
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            put(MediaStore.Images.Media.WIDTH, bmp.width)
+            put(MediaStore.Images.Media.HEIGHT, bmp.height)
+        }
+        return try {
+            val contentResolver = fragment.requireContext().contentResolver
+            contentResolver.insert(imageCollection, contentValues)?.also { uri ->
+                contentResolver.openOutputStream(uri).use { outputStream ->
+                    if (!bmp.compress(Bitmap.CompressFormat.JPEG, 95, outputStream)) {
+                        throw IOException("Couldn't save bitmap")
+                    }
+                }
+            } ?: throw IOException("Couldn't create MediaStore entry")
+            true
+        } catch (e: IOException) {
+            e.printStackTrace()
+            false
+        }
     }
 
     companion object {
